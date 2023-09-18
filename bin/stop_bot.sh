bc3dcef1b4d68345bd2e9df86d87f2467334c6ab
han3on <70957964+han3on@users.noreply.github.com>
2023-09-18 09:50:30 +0800

    Update stop_bot.sh

--- bin/stop_bot.sh
+++ bin/stop_bot.sh
@@ -5,9 +5,9 @@
 pid=$(ps -ef | grep "telegram_message_parser.py" | grep -v grep | awk '{print $2}')
 
 if [ -z "$pid" ]; then
-  echo "Bot is not running"
+  echo "Bot is not running."
 else
   # Terminate the process
   kill $pid
-  echo "Bot has been terminated"
+  echo "Bot has been terminated."
 fi
