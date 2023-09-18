96e5d7e96653318feead87a84c6f28679b8610ce
han3on <70957964+han3on@users.noreply.github.com>
2023-09-18 09:49:51 +0800

    Update restart_bot.sh

--- bin/restart_bot.sh
+++ bin/restart_bot.sh
@@ -6,12 +6,12 @@ echo "Restarting bot..."
 pid=`ps -ef | grep telegram_message_parser.py | grep -v grep | awk '{print $2}'`
 
 if [ -z $pid ]; then
-    echo "Bot is not running"
+    echo "Bot is not running."
 else
-    echo "Bot has been terminated"
+    echo "Bot has been terminated."
     kill -9 $pid
 fi
 
 echo "Starting bot..."
 nohup python3 src/telegram_message_parser.py >/dev/null 2>&1 &
-echo "Bot has been restarted successfully"
+echo "Bot has been restarted successfully."
